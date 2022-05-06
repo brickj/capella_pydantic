d1bbb25e6425ed9b2f2a300a18fa81e807142eb6
brickj <resumerickjacobs@yahoo.com>
2022-05-06 17:56:58 -0400

    Add files via upload

---
+++ capella_pydantic/src/pydantic_capella.py
@@ -0,0 +1,70 @@
+# Pydantic Imports
+from pydantic import BaseModel
+from typing import List, Optional
+
+# Capella Imports
+from couchbase.cluster import Cluster, ClusterOptions
+from couchbase.cluster import PasswordAuthenticator
+from couchbase.cluster import QueryOptions
+from couchbase.management.queries import CreatePrimaryQueryIndexOptions
+
+
+class Review(BaseModel):
+    author: str
+    review: str
+    likes: int
+
+
+class UserPost(BaseModel):
+    author: str
+    date: str
+    title: Optional[str] = None
+    content: str
+    id: int
+    likes: List[str]
+    reviews: List[Review]
+
+
+review = [
+    Review(author="johndoe", review="This is a comment!", likes=3),
+    Review(author="rickJ", review="This is a Rick J comment!", likes=1),
+    Review(author="janedoe", review="This is a Jane Doe comment!", likes=2)
+
+]
+
+user_post1 = UserPost(author="johndoe",
+                      date="1/1/1970",
+                      title="Cool post",
+                      content="Cool content",
+                      id=10101,
+                      likes=["johndoe", "janedoe"],
+                      reviews=review)
+
+#Print UserPost
+print(user_post1)
+#Print first review
+print(user_post1.reviews[0].review)
+
+
+def capella_connect():
+    endpoint = 'cb.915ukpztrpvozhqn.cloud.couchbase.com'
+    username = "db_user2"
+    password = "PassWord1!"
+    bucketName = 'pydantic'
+    #### User Input ends here.
+    # Initialize the Connection
+    cluster = Cluster('couchbases://' + endpoint + '?ssl=no_verify',
+                      ClusterOptions(PasswordAuthenticator(username, password)))
+    cb = cluster.bucket(bucketName)
+    cb_coll = cb.default_collection()
+    # Create a N1QL Primary Index (but ignore if it exists)
+    cluster.query_indexes().create_primary_index(bucketName, ignore_if_exists=True)
+
+    # Upsert JSON dict with key 'u:pydantic_document'
+    cb_coll.upsert('u:pydantic_document', user_post1.dict())
+
+    # Load the Document and print it
+    print(cb_coll.get('u:pydantic_document').content_as[str])
+
+
+capella_connect()
